e246aa68b7451410336d38fc4fc2a105732afa32
Lionel Bijaoui <lionel.bijaoui@gmail.com>
2025-12-01 17:35:18 +0100

    fix: change station type from int to Integer in FireStationDTO for nullability

--- src/main/java/com/openclassrooms/safetynet/alert/dto/FireStationDTO.java
+++ src/main/java/com/openclassrooms/safetynet/alert/dto/FireStationDTO.java
@@ -4,4 +4,4 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 /** Data Transfer Object for Fire Station information. */
-public record FireStationDTO(@NotBlank String address, @NotNull int station) {}
+public record FireStationDTO(@NotBlank String address, @NotNull Integer station) {}
