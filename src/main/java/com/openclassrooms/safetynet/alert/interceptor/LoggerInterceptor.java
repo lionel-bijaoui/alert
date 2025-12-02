3a850ddf0dc190ee6dfa2b517a08bdec7af34e9f
Lionel Bijaoui <lionel.bijaoui@gmail.com>
2025-12-02 20:20:40 +0100

    fix: add nullable annotations to HttpServletResponse and handler parameters in LoggerInterceptor

--- src/main/java/com/openclassrooms/safetynet/alert/interceptor/LoggerInterceptor.java
+++ src/main/java/com/openclassrooms/safetynet/alert/interceptor/LoggerInterceptor.java
@@ -1,5 +1,6 @@
 package com.openclassrooms.safetynet.alert.interceptor;
 
+import jakarta.annotation.Nullable;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -7,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 /** Interceptor to log incoming HTTP requests and outgoing HTTP responses. */
 @Slf4j
@@ -16,7 +16,9 @@ public class LoggerInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(
-            HttpServletRequest request, HttpServletResponse response, Object handler) {
+            HttpServletRequest request,
+            @Nullable HttpServletResponse response,
+            @Nullable Object handler) {
         // Log basic request information
         log.info(
                 "Incoming request: {} {}{} from {}",
@@ -29,17 +31,10 @@ public class LoggerInterceptor implements HandlerInterceptor {
     }
 
     @Override
-    public void postHandle(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            Object handler,
-            ModelAndView modelAndView) {}
-
-    @Override
     public void afterCompletion(
             HttpServletRequest request,
             HttpServletResponse response,
-            Object handler,
+            @Nullable Object handler,
             Exception ex) {
         // Log response status
         log.info(
