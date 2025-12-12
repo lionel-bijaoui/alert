09a99c971abd427884772120a5f5f930ad4cfca4
Lionel Bijaoui <lionel.bijaoui@gmail.com>
2025-12-12 20:11:13 +0100

    docs: add JavaDoc for PopulationByFireStationDTO and PopulationByFireStationsDTO

--- src/main/java/com/openclassrooms/safetynet/alert/dto/PopulationByFireStationDTO.java
+++ src/main/java/com/openclassrooms/safetynet/alert/dto/PopulationByFireStationDTO.java
@@ -2,5 +2,9 @@ package com.openclassrooms.safetynet.alert.dto;
 
 import java.util.List;
 
+/**
+ * Data Transfer Object for population information by fire station, including counts of adults and
+ * children.
+ */
 public record PopulationByFireStationDTO(
         List<PersonSummaryDTO> population, int adultCount, int childCount) {}
--- src/main/java/com/openclassrooms/safetynet/alert/dto/PopulationByFireStationsDTO.java
+++ src/main/java/com/openclassrooms/safetynet/alert/dto/PopulationByFireStationsDTO.java
@@ -2,5 +2,6 @@ package com.openclassrooms.safetynet.alert.dto;
 
 import java.util.List;
 
+/** Data Transfer Object for population information by fire stations. */
 public record PopulationByFireStationsDTO(
         List<Integer> fireStationNumbers, List<PersonWithMedicalInfosDTO> population) {}
